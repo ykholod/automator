60fbc0f13ffee3a48d102f0f10c8c47892a9fb39
Yaroslav Kholod <pretorian.yaroslav@gmail.com>
2019-05-05 16:57:28 +0300

    some changes

--- automator.py
+++ automator.py
@@ -17,8 +17,6 @@
 import argparse
 import os
 
-# Yo-ho-ho
-
 
 parser = argparse.ArgumentParser(description='The Project build, test and launch point')
 subparsers = parser.add_subparsers(help='List of commands')
